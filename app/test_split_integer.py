0bb44a05bbd09c1903f1342509481becaf2217ff
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-15 13:25:18 -0300

    Solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,23 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    expected = [5, 5, 5]
-    actual = split_integer(15, 3)
-    assert actual == expected
+    assert split_integer(15, 3) == [5, 5, 5]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    expected = [4, 4, 4, 4]
-    actual = split_integer(16, 4)
-    assert actual == expected
+    assert split_integer(6, 2) == [3, 3]
+    assert split_integer(16, 4) == [4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    expected = [5]
-    actual = split_integer(5, 1)
-    assert actual == expected
+    assert split_integer(5, 1) == [5]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    expected = [5, 5, 5, 5, 6, 6]
-    actual = split_integer(32, 6)
-    assert actual == expected
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    expected = [0, 1, 1, 1, 1]
-    actual = split_integer(4, 5)
-    assert actual == expected
+    assert split_integer(2, 3) == [0, 1, 1]
