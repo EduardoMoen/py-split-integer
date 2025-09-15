4ee7090910e81bc67e75b305c964a4d1c52d9872
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-15 15:57:33 -0300

    Solution3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from app.split_integer import split_integer
 
 
@@ -13,12 +14,18 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
         (15, 3, [5, 5, 5]),
     ]
 )
-def test_result_properties_for_various_inputs(value, parts, result) -> None:
+def test_result_properties_for_various_inputs(
+    value: int,
+    parts: int,
+    result: list,
+) -> None:
     assert len(split_integer(value, parts)) == parts
     assert all(isinstance(x, int) for x in split_integer(value, parts))
     assert split_integer(value, parts) == sorted(split_integer(value, parts))
-    assert max(split_integer(value, parts)) - min(split_integer(value, parts)) <= 1
+    assert (max(split_integer(value, parts))
+            - min(split_integer(value, parts)) <= 1)
     assert sum(split_integer(value, parts)) == value
+
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
